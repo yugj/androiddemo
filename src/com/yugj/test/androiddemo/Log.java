ee4dd6857b09ebfa9e58ddde4b368d351dd5d7fa
yugj <yugj@hotmail.com>
2015-12-21 23:28:29 +0800

    http

--- src/com/yugj/test/androiddemo/Log.java
+++ src/com/yugj/test/androiddemo/Log.java
@@ -1,5 +1,5 @@
 package com.yugj.test.androiddemo;
 
 public class Log {
-
+	
 }
--- src/com/yugj/test/androiddemo/S.java
+++ src/com/yugj/test/androiddemo/S.java
@@ -1,6 +1,12 @@
 package com.yugj.test.androiddemo;
 
+import android.util.Log;
+
 public class S {
 	public static final String tag = "android_demo";
+	
+	public static void o(String obj) { 
+		Log.i(tag,obj);
+	}
 
 }
